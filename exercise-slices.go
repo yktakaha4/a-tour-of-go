cb8f6bd3bb012d6d92af786ebfabaa564df1740a
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-05-23 11:31:07 +0900

    gofmt

--- exercise-slices.go
+++ exercise-slices.go
@@ -6,14 +6,14 @@ import (
 
 func Pic(dx, dy int) [][]uint8 {
 	pic := make([][]uint8, dy)
-	
-	for y := range(pic) {
+
+	for y := range pic {
 		pic[y] = make([]uint8, dx)
-		for x := range(pic[y]) {
+		for x := range pic[y] {
 			pic[y][x] = uint8(x * y)
 		}
 	}
-	
+
 	return pic
 }
 
